30932276cdda9444c2ecf6cf6edead727e4f00f5
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2025-01-05 23:38:05 +0000

    Timeline

--- src/Game_Components/DecisionTimeLine.jsx
+++ src/Game_Components/DecisionTimeLine.jsx
@@ -4,28 +4,30 @@ import "./DecisionTimeLine.css"; // Para estilos personalizados
 const Decisions = () => {
   return (
     <div>
-       <div className="background-video">
-                <video autoPlay muted loop>
-                    <source src="./backgroundClip.mp4" type="video/mp4" />
-                </video>
-            </div>
-    <div className="container">
-      <div className="problem-box">(Problema)</div>
-      <div className="decision-box">(Decisão tomada pelo Jogador)</div>
-      <div className="stats">
-        <div className="stat">
-          <span className="arrow up">↑</span> +15 Social
-        </div>
-        <div className="stat">
-          <span className="arrow down">↓</span> -10 Ecológico
-        </div>
+      <div className="background-video">
+        <video autoPlay muted loop>
+          <source src="./backgroundClip.mp4" type="video/mp4" />
+        </video>
       </div>
-      <button className="nav-button left">←</button>
-      <button className="nav-button right">→</button>
-    </div>
+      <div className="container">
+        <div className="problem-box">(Problema)</div>
+        <div className="decision-box">(Decisão tomada pelo Jogador)</div>
+        <div className="stats">
+          <div className="stat">
+            <span className="arrow up">↑</span> +15 Social
+          </div>
+          <div className="stat">
+            <span className="arrow down">↓</span> -10 Ecológico
+          </div>
+          <div className="stat">
+            <span className="arrow environment">⬆</span> +20 Ambiental
+          </div>
+        </div>
+        <button className="nav-button left">←</button>
+        <button className="nav-button right">→</button>
+      </div>
     </div>
   );
-}
+};
 
 export default Decisions;
-
