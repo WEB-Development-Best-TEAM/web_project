f91d6983c4a0fb6b9f14effa7efb36453c60a949
GONCAVF <veloso.goncalo@gmail.com>
2025-01-06 22:55:48 +0000

    Fix Register Information

--- src/EntryScreen_Components/RegisterScreen.jsx
+++ src/EntryScreen_Components/RegisterScreen.jsx
@@ -10,8 +10,8 @@ const RegisterScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [success, setSuccess] = useState('');
   const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -43,7 +43,7 @@ const RegisterScreen = () => {
         await setDoc(doc(db, "users", user.uid), {
             uid: user.uid,
             username,
-            email,             
+            email,
             registrationDate: new Date().toISOString(),
         });
 
@@ -53,68 +53,69 @@ const RegisterScreen = () => {
         setError('Erro ao registar utilizador: ' + error.message);
         setSuccess('');
     }
-};
+  };
 
   return (
     <form onSubmit={handleRegister}>
-      <div className="background-video">
-        <video autoPlay muted loop>
-          <source src="./backgroundClip.mp4" type="video/mp4" />
-        </video>
-      </div>
-      <h1>Registrar</h1>
+        <div className="background-video">
+            <video autoPlay muted loop>
+                <source src="./backgroundClip.mp4" type="video/mp4" />
+            </video>
+        </div>
+        <h1>Registrar</h1>
 
-      <div className="inputBox">
-        <input
-          type="text"
-          placeholder="Nome de Usuário"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <img className="icon" src="/user_icon.png" alt="User Icon" />
-      </div>
+        <div className="inputBox">
+            <input
+                type="text"
+                placeholder="Nome de Utilizador"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+            <img className="icon" src={"/user_icon.png"} alt="User Icon" />
+        </div>
 
-      <div className="inputBox">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <img className="icon" src="/user_icon.png" alt="Email Icon" />
-      </div>
+        <div className="inputBox">
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <img className="icon" src={"/user_icon.png"} alt="Email Icon" />
+        </div>
 
-      <div className="inputBox">
-        <input
-          type="password"
-          placeholder="Palavra-Passe"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <img className="icon" src="/password_icon.png" alt="Password Icon" />
-      </div>
+        <div className="inputBox">
+            <input
+                type="password"
+                placeholder="Palavra-Passe"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <img className="icon" src={"/password_icon.png"} alt="Password Icon" />
+        </div>
 
-      <div className="inputBox">
-        <input
-          type="password"
-          placeholder="Confirmar Palavra-Passe"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-        <img className="icon" src="/password_icon.png" alt="Password Icon" />
-      </div>
+        <div className="inputBox">
+            <input
+                type="password"
+                placeholder="Confirmar Palavra-Passe"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+            />
+            <img className="icon" src={"/password_icon.png"} alt="Password Icon" />
+        </div>
 
-      {error && <p>{error}</p>}
+        {error && <p>{error}</p>}
+        {success && <p>{success}</p>}
 
-      <p>Já tenho uma conta, <Link to="/LoginScreen">entrar</Link>.</p>
+        <p>Já tenho uma conta, <Link to="/LoginScreen">entrar</Link>.</p>
 
-      <input type="submit" value="Registrar" />
+        <input type="submit" value="Registrar" />
     </form>
-  );
-};
+);
+}
 
 export default RegisterScreen;
